29c0edd0a1af1c639ec2e56622100dcf66684e53
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-13 00:42:53 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+def _requires_from_file(filename):
+    return open(filename, encoding="utf8").read().splitlines()
+
+setuptools.setup(
+    name="sanic-aioMySQL",
+    version="2022.4.13",
+    author="DMS",
+    author_email="masato190411@gmail.com",
+    description="Easy to use MySQL for sanic.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tuna2134/sanic-aioMySQL",
+    install_requires=_requires_from_file('rqs.txt'),
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
