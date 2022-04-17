066b1dc1bb389db522dda4c5cb6654c633d44ea1
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-17 19:25:35 +0900

    Create main.py

---
+++ tests/main.py
@@ -0,0 +1,23 @@
+import pytest
+import sanic_mysql
+from sanic_testing import TestManager
+from sanic import Sanic, response
+
+@pytest.fixture
+def app():
+    sanic_app = Sanic("TestSanic")
+    TestManager(sanic_app)
+    sanic_mysql.ExtendMySQL(app, host="", user="", password="")
+    
+    @sanic_app.get("/")
+    @sanic_mysql.cursor()
+    def basic(request, connection, cursor):
+        await cursor.execute("SELECT * FROM test")
+        return response.text("foo")
+    return sanic_app
+    
+def test_basic_test_client(app):
+    request, response = app.test_client.get("/")
+    assert request.method.lower() == "get"
+    assert response.body == b"foo"
+    assert response.status == 200
